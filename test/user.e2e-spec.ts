1d0ed8369639169a6b9df1db38100311dcaa2b0a
lucas <lucaslpa12345@gmail.com>
2024-10-06 17:42:53 -0300

    test(user modifier): adding integration tests

--- test/user.e2e-spec.ts
+++ test/user.e2e-spec.ts
@@ -2,22 +2,51 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { UserModule } from './../src/modules/User/UserModule';
-import { UserInput } from './../src/application/dto/UserInput';
-import { ERole } from 'src/business/interfaces/ERole';
+import { UserInput } from '@application';
+import { ERole } from '@business';
+import { FirebaseModule } from './../src/firebase.module';
+import { ConfigModule } from '@nestjs/config';
 
 describe('UserModule (e2e)', () => {
   let app: INestApplication;
+  let userId: string;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [UserModule],
+      imports: [
+        ConfigModule.forRoot({
+          isGlobal: true,
+          envFilePath: '.env',
+        }),
+        UserModule,
+        FirebaseModule,
+      ],
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
   });
 
-  it('should create a user', () => {
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('should create a user', async () => {
+    const input: UserInput = {
+      name: 'joaozinho',
+      email: 'joa2323o@gmail.com',
+      password: '12345678',
+      role: ERole.enterprise,
+      id: '',
+    };
+    const response = await request(app.getHttpServer())
+      .post('/user/signup')
+      .send(input);
+    expect(response.status).toBe(200);
+    userId = response.body.data.id;
+  });
+
+  it('should fail to create a user with the same email', async () => {
     const input: UserInput = {
       name: 'joão',
       email: 'joa2323o@gmail.com',
@@ -25,6 +54,44 @@ describe('UserModule (e2e)', () => {
       role: ERole.enterprise,
       id: '',
     };
-    return request(app.getHttpServer()).post('/signup').send(input).expect(201);
+    const response = await request(app.getHttpServer())
+      .post('/user/signup')
+      .send(input);
+    expect(response.status).toBe(400);
+  });
+
+  it('should fail to create a user with the wrong email', async () => {
+    const input: UserInput = {
+      name: 'joão',
+      email: 'joa2323o@',
+      password: '12345',
+      role: ERole.enterprise,
+      id: '',
+    };
+    const response = await request(app.getHttpServer())
+      .post('/user/signup')
+      .send(input);
+    expect(response.status).toBe(400);
+  });
+
+  it('should update the user', async () => {
+    const updateInput = {
+      id: userId,
+      name: 'joaozinho updated',
+      email: 'joa2323o_updated@gmail.com',
+    };
+    const response = await request(app.getHttpServer())
+      .put(`/user/update/${userId}`)
+      .send(updateInput);
+    expect(response.status).toBe(200);
+    expect(response.body.data.name).toBe(updateInput.name);
+    expect(response.body.data.email).toBe(updateInput.email);
+  });
+
+  it('should delete the user', async () => {
+    const response = await request(app.getHttpServer()).delete(
+      `/user/exclude/${userId}`,
+    );
+    expect(response.status).toBe(200);
   });
 });
